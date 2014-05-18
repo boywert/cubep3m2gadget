8f68a1d8453dc7939751337e506adbb5ea8267f2
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-19 00:07:01 +0100

    autoupdate

--- intbitmask.c
+++ intbitmask.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+void highword(long *longword, int *high);
+void lowword(long *longword, int *low);
 void highword(long *longword, int *high)
 {
   (*high) =((*longword) >> 32);
