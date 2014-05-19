3fd52f51491e80e9cb88760b2fe07cf15d1efee5
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-19 10:13:56 +0100

    autoupdate

--- cubep3m2gadget.f90
+++ cubep3m2gadget.f90
@@ -37,106 +37,105 @@ program test
   if (ierr /= mpi_success) call mpi_abort(mpi_comm_world,ierr,ierr)
   call mpi_comm_rank(mpi_comm_world,rank,ierr)
   if (ierr /= mpi_success) call mpi_abort(mpi_comm_world,ierr,ierr)
-  ! if(totalnodes < num_files) then
-  !    if(rank == 0) then
-  !       print*, "total nodes:",totalnodes,"total files:",num_files
-  !       print*, "aborting"
-  !       call abort
-  !    endif
-  ! endif
-  if(rank < num_files) then
-     
-     write(z_s, "(f10.3)") redshift
-     z_s = adjustl(z_s)
-     write(str_rank, "(I10)") rank
-     str_rank = adjustl(str_rank)
-     xv_input = "/scratch/00506/ilievit/cubepm_130315_6_1728_47Mpc_ext2/results/"//z_s(1:len_trim(z_s))//"xv"//str_rank(1:len_trim(str_rank))//".dat"
-     pid_input = "/scratch/00506/ilievit/cubepm_130315_6_1728_47Mpc_ext2/results/"//z_s(1:len_trim(z_s))//"PID"//str_rank(1:len_trim(str_rank))//".dat"
-     output = "/scratch/01937/cs390/test/"//z_s(1:len_trim(z_s))//"/"
-     call system("mkdir -p "//trim(output))
-     output = trim(output)//"/"//z_s(1:len_trim(z_s))//"."//str_rank(1:len_trim(str_rank))
-     
-     do k=0,ngdim-1
-        do j=0,ngdim-1
-           do i=0,ngdim-1
-              if(rank == i+j*ngdim+k*ngdim**2) then
-                 nc_offset(1) = real(i*ncdim/ngdim)
-                 nc_offset(2) = real(j*ncdim/ngdim)
-                 nc_offset(3) = real(k*ncdim/ngdim)
-              endif
-           enddo
+  if(totalnodes == num_files) then
+     if(rank == 0) then
+        print*, "total nodes:",totalnodes,"total files:",num_files
+        print*, "aborting"
+        call abort
+     endif
+  endif
+
+  write(z_s, "(f10.3)") redshift
+  z_s = adjustl(z_s)
+  write(str_rank, "(I10)") rank
+  str_rank = adjustl(str_rank)
+  xv_input = "/scratch/00506/ilievit/cubepm_130315_6_1728_47Mpc_ext2/results/"//z_s(1:len_trim(z_s))//"xv"//str_rank(1:len_trim(str_rank))//".dat"
+  pid_input = "/scratch/00506/ilievit/cubepm_130315_6_1728_47Mpc_ext2/results/"//z_s(1:len_trim(z_s))//"PID"//str_rank(1:len_trim(str_rank))//".dat"
+  output = "/scratch/01937/cs390/test/"//z_s(1:len_trim(z_s))//"/"
+  call system("mkdir -p "//trim(output))
+  output = trim(output)//"/"//z_s(1:len_trim(z_s))//"."//str_rank(1:len_trim(str_rank))
+
+  do k=0,ngdim-1
+     do j=0,ngdim-1
+        do i=0,ngdim-1
+           if(rank == i+j*ngdim+k*ngdim**2) then
+              nc_offset(1) = real(i*ncdim/ngdim)
+              nc_offset(2) = real(j*ncdim/ngdim)
+              nc_offset(3) = real(k*ncdim/ngdim)
+           endif
         enddo
      enddo
+  enddo
 #define EXTRAPID
 
 #ifdef EXTRAPID
-     open(unit=21,file=trim(xv_input),status='old',form='binary')
-     read(21) np_local,a,t,tau,nts,dt_f_acc,dt_pp_acc,dt_c_acc,cur_checkpoint, &
-          cur_projection,cur_halofind,mass_p
+  open(unit=21,file=trim(xv_input),status='old',form='binary')
+  read(21) np_local,a,t,tau,nts,dt_f_acc,dt_pp_acc,dt_c_acc,cur_checkpoint, &
+       cur_projection,cur_halofind,mass_p
 
-     allocate(PID(1:np_local))
-     allocate(xv(1:6,1:np_local))
+  allocate(PID(1:np_local))
+  allocate(xv(1:6,1:np_local))
 
-     read(21) xv(:,:np_local)
-     close(21)
+  read(21) xv(:,:np_local)
+  close(21)
 
-     open(unit=21,file=trim(pid_input),status='old',form='binary')
-     read(21) np_local,a,t,tau,nts,dt_f_acc,dt_pp_acc,dt_c_acc,cur_checkpoint, &
-          cur_projection,cur_halofind,mass_p
-     if (rank==0) print*, np_local,a,t,tau,nts,dt_f_acc,dt_pp_acc,dt_c_acc,cur_checkpoint, &
-          cur_projection,cur_halofind,mass_p
+  open(unit=21,file=trim(pid_input),status='old',form='binary')
+  read(21) np_local,a,t,tau,nts,dt_f_acc,dt_pp_acc,dt_c_acc,cur_checkpoint, &
+       cur_projection,cur_halofind,mass_p
+  if (rank==0) print*, np_local,a,t,tau,nts,dt_f_acc,dt_pp_acc,dt_c_acc,cur_checkpoint, &
+       cur_projection,cur_halofind,mass_p
 
 
-     read(21) PID
-     close(21)
-     ! convert cubep3m units -> gadget units 
-     do i=1,3
-        xv(i,1:np_local) = ((xv(i,1:np_local) + nc_offset(i)))*boxsize/real(ncdim)
-     enddo
-     xv(4:6,1:np_local) = xv(4:6,1:np_local)/sqrt(a)
-     
+  read(21) PID
+  close(21)
+  ! convert cubep3m units -> gadget units 
+  do i=1,3
+     xv(i,1:np_local) = ((xv(i,1:np_local) + nc_offset(i)))*boxsize/real(ncdim)
+  enddo
+  xv(4:6,1:np_local) = xv(4:6,1:np_local)/sqrt(a)
+
 #endif
 
-     g_time = a
+  g_time = a
 
-     g_mass(1:6) = 0.0d0
-     g_mass(2) = real(mass_p,8)
-     g_npart(1:6) = 0
-     g_npart(2) = np_local
-     mpi_npart = int(np_local,8)
+  g_mass(1:6) = 0.0d0
+  g_mass(2) = real(mass_p,8)
+  g_npart(1:6) = 0
+  g_npart(2) = np_local
+  mpi_npart = int(np_local,8)
 
-     g_flag_sfr = 0
-     g_flag_feedback = 0
-     g_flag_cooling = 0
-     g_num_files = num_files
-     g_Boxsize = boxsize
-     g_Omega0 = Omega0
-     g_redshift = redshift
-     g_OmegaLambda = OmegaLambda
-     g_HubbleParam = HubbleParam
+  g_flag_sfr = 0
+  g_flag_feedback = 0
+  g_flag_cooling = 0
+  g_num_files = num_files
+  g_Boxsize = boxsize
+  g_Omega0 = Omega0
+  g_redshift = redshift
+  g_OmegaLambda = OmegaLambda
+  g_HubbleParam = HubbleParam
 
-     call MPI_BARRIER(MPI_COMM_WORLD,ierr)
-     call mpi_allreduce(mpi_npart,mpi_nparttotal,1,mpi_integer8,mpi_sum,mpi_comm_world,ierr)
-     call MPI_BARRIER(MPI_COMM_WORLD,ierr)
+  call MPI_BARRIER(MPI_COMM_WORLD,ierr)
+  call mpi_allreduce(mpi_npart,mpi_nparttotal,1,mpi_integer8,mpi_sum,mpi_comm_world,ierr)
+  call MPI_BARRIER(MPI_COMM_WORLD,ierr)
 
-     g_npartTotal(1:6) = 0
-     g_nhighword(1:6) = 0
-     if(rank == 0 ) print*,"total N",mpi_nparttotal
-     g_npartTotal(2) = iand(mpi_nparttotal,2**32-1)
-     if(rank == 0) print*, g_npartTotal
-     g_nhighword(2) = ishft(mpi_nparttotal,-32)
-     if(rank == 0) print*,g_nhighword
-     open(unit=21,file=trim(output),form='unformatted')
+  g_npartTotal(1:6) = 0
+  g_nhighword(1:6) = 0
+  if(rank == 0 ) print*,"total N",mpi_nparttotal
+  g_npartTotal(2) = iand(mpi_nparttotal,2**32-1)
+  if(rank == 0) print*, g_npartTotal
+  g_nhighword(2) = ishft(mpi_nparttotal,-32)
+  if(rank == 0) print*,g_nhighword
+  open(unit=21,file=trim(output),form='unformatted')
 
-     write(21) g_npart, g_mass, g_time, g_redshift, g_flag_sfr, g_flag_feedback, g_npartTotal, &
-          g_flag_cooling, g_num_files, g_boxsize, g_Omega0, g_OmegaLambda, g_HubbleParam, &
-          g_flag_stellarage, g_flag_metals, g_nhighword, g_filler
-     write(21) xv(1:3,1:np_local)
-     write(21) xv(4:6,1:np_local)
-     write(21) PID(1:np_local)
-     close(21)
+  write(21) g_npart, g_mass, g_time, g_redshift, g_flag_sfr, g_flag_feedback, g_npartTotal, &
+       g_flag_cooling, g_num_files, g_boxsize, g_Omega0, g_OmegaLambda, g_HubbleParam, &
+       g_flag_stellarage, g_flag_metals, g_nhighword, g_filler
+  write(21) xv(1:3,1:np_local)
+  write(21) xv(4:6,1:np_local)
+  write(21) PID(1:np_local)
+  close(21)
 
-     deallocate(xv,PID)
-  endif
+  deallocate(xv,PID)
+
   call mpi_finalize(ierr)
 end program test
