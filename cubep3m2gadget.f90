f4d3347083875d908bc43d82d4d1d457c54d5aa6
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-19 06:00:49 +0100

    autoupdate

--- cubep3m2gadget.f90
+++ cubep3m2gadget.f90
@@ -84,7 +84,7 @@ program test
   
   g_npartTotal(1:6) = 0
   g_nhighword(1:6) = 0
-  g_npartTotal(2) = iand(mpi_comm_world,1111 1111 1111 1111 1111 1111 1111 1111)
+  g_npartTotal(2) = iand(mpi_comm_world,2**32)
   g_nhighword(2) = ishft(mpi_nparttotal,32)
   open(unit=21,file=trim(output),form='unformatted')
   
