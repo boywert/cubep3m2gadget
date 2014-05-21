406be2e2f381f911adebee269d61a93c3de6adc9
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-21 22:14:02 +0100

    autoupdate

--- read_catalogues.c
+++ read_catalogues.c
@@ -335,5 +335,5 @@ long gadget_load_snapshot(char *fname, int files)
 
 int main ()
 {
-  printf("%ld\n",gadget_load_snapshot("/scratch/01937/cs390/cubepm_130315_6_1728_47Mpc_ext2/cube2gadget",216));
+  printf("%ld\n",gadget_load_snapshot("/scratch/01937/cs390/cubepm_130315_6_1728_47Mpc_ext2/snapdir_000/cube2gadget_000",216));
 }
