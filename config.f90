286c5823dc63e1eb45bf25d684df8fd136d677de
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-12-08 12:10:25 +0000

    test

--- config.f90
+++ config.f90
@@ -1,8 +1,8 @@
-  npdim = 1728           !cubep3m nc
-  ngdim = 6              !cubep3m nd
+  npdim = 4000           !cubep3m nc
+  ngdim = 10              !cubep3m nd
   OmegaLambda = 0.73      
   Omega0 = 0.27
   HubbleParam = 0.7
-  boxsize = 47.0         !Mpc/h
+  boxsize = 244.0         !Mpc/h
   input_dir = "/ccc/cont005/home/ra1089/srisawac/ilian_dir/cubepm_130329_10_4000_244Mpc_ext2_test/results/"
   output_dir = "/ccc/scratch/cont005/ra1089/srisawac/cubepm_130329_10_4000_244Mpc_ext2_test/gadget/"
